8ee68f60b80ce37af07b6d7dc5a2dfedc148e49f
Pooria Setayesh <pooriaset@yahoo.com>
2024-12-12 18:30:35 +0330

    feat: add total counts and headless to the plugin

--- engine/Base.php
+++ engine/Base.php
@@ -31,7 +31,55 @@ class Base
 	public function initialize()
 	{
 		$this->settings = \s_get_settings();
+		add_action('wp', array($this, 'headlesswp_frontend_redirect'));
 
 		return true;
+	}
+
+	/**
+	 * Die if we try to access a page or the front page
+	 *
+	 * @return void
+	 */
+	public static function headlesswp_frontend_redirect()
+	{
+		if (! is_admin()) {
+
+			/**
+			 * Fetch the IDs of the post, page or blog page
+			 */
+			$post_ID     = get_the_id();
+			$homepage_id = get_option('page_on_front');
+			$blogpage_id = get_option('page_for_posts');
+
+			/**
+			 * Do a wp_die so we can't access the site
+			 */
+			if ($homepage_id === $post_ID || $blogpage_id === $post_ID || is_front_page()) {
+				wp_die('This site is not accessible');
+				exit;
+			} else {
+
+				/**
+				 * Else do a redirect back to WP admin again
+				 */
+
+				$post_edit_link = admin_url('post.php?post=' . $post_ID . '&action=edit');
+
+				if (is_user_logged_in()) {
+					/**
+					 * Logged in users go to the post edit screen
+					 */
+					wp_safe_redirect($post_edit_link);
+					exit;
+				} else {
+					/**
+					 * Not logged in? Redirect to login page
+					 */
+					wp_safe_redirect(wp_login_url($post_edit_link));
+					exit;
+				}
+			}
+		}
 	}
 }
---
+++ graphql/TotalCount.php
@@ -0,0 +1,85 @@
+<?php
+
+/**
+ * nextjs_woo_plugin
+ *
+ * @package   nextjs_woo_plugin
+ * @author    Pooria Setayesh <pooriaset@yahoo.com>
+ * @copyright 2022 Shop
+ * @license   GPL 2.0+
+ * @link      
+ */
+
+namespace nextjs_woo_plugin\Graphql;
+
+use nextjs_woo_plugin\Engine\Base;
+
+
+class TotalCount extends Base
+{
+
+    /**
+     * Initialize the class.
+     *
+     * @return void|bool
+     */
+    public function initialize()
+    {
+        parent::initialize();
+
+        if (class_exists('WPGraphQL')) {
+            add_filter('graphql_connection_page_info', array($this, 'resolve_total_field'), 10, 2);
+            add_filter('graphql_connection_query_args', array($this, 'count_total_rows'));
+            add_filter('graphql_register_types', array($this, 'register_total_field'));
+        }
+    }
+
+    /**
+     * Resolve the total field.
+     *
+     * @param $page_info
+     * @param $connection
+     *
+     * @return mixed
+     */
+    public function resolve_total_field($page_info, $connection)
+    {
+        $page_info['total'] = null;
+        if ($connection->get_query() instanceof \WP_Query) {
+            if (isset($connection->get_query()->found_posts)) {
+                $page_info['total'] = (int) $connection->get_query()->found_posts;
+            }
+        } elseif ($connection->get_query() instanceof \WP_User_Query) {
+            if (isset($connection->get_query()->total_users)) {
+                $page_info['total'] = (int) $connection->get_query()->total_users;
+            }
+        }
+
+        return $page_info;
+    }
+
+    /**
+     * Tell the underlying WP_Query to count the total number of rows.
+     *
+     * @param $args
+     *
+     * @return mixed
+     */
+    public function count_total_rows($args)
+    {
+        $args['no_found_rows'] = false;
+        $args['count_total']   = true;
+
+        return $args;
+    }
+
+    /**
+     * Register a total field for queries.
+     */
+    public function register_total_field()
+    {
+        register_graphql_field('WPPageInfo', 'total', [
+            'type' => 'Int',
+        ]);
+    }
+}
