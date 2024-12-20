19c3b5c26b385393d5543dc0f68a6943a6e3a59e
Pooria Setayesh <pooriaset@yahoo.com>
2024-12-20 04:38:54 +0330

    chore: increase time of expiration

--- graphql/GraphqlWoo.php
+++ graphql/GraphqlWoo.php
@@ -27,6 +27,8 @@ class GraphqlWoo extends Base
     {
         parent::initialize();
         add_filter('graphql_is_generate_woocommerce_session_token', array($this, "graphql_is_generate_woocommerce_session_token"), 10);
+        add_filter('wc_session_expiring', array($this, "wc_session_expiring"), 10);
+        add_filter('wc_session_expiration', array($this, "wc_session_expiration"), 10);
     }
 
     public function graphql_is_generate_woocommerce_session_token()
@@ -35,4 +37,17 @@ class GraphqlWoo extends Base
         $headers = getallheaders();
         return  $method !== "OPTIONS" && !isset($headers['x-server-side']);
     }
+
+    public function wc_session_expiring()
+    {
+        return time() +   60 * 60 * 24 * 7 - 1;
+    }
+
+
+    public function wc_session_expiration()
+    {
+        return time() +   60 * 60 * 24 * 7;
+    }
 }
+
+
