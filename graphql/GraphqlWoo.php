7138cec65cc65ca02e6094990f07a52a8f2ce213
Pooria Setayesh <pooriasetayesh@gmail.com>
2025-01-12 15:01:29 +0330

    chore: resolve errors of plugin

--- graphql/GraphqlWoo.php
+++ graphql/GraphqlWoo.php
@@ -31,18 +31,33 @@ class GraphqlWoo extends Base
         add_filter('wc_session_expiration', array($this, "wc_session_expiration"), 10);
     }
 
+    /**
+     * Condition of generating new token.
+     *
+     * @return bool
+     */
     public function graphql_generate_woocommerce_session_token_condition()
     {
         $headers = getallheaders();
         return  isset($headers['x-generate-session']);
     }
 
+    /**
+     * Woocommerce session expiring.
+     *
+     * @return int
+     */
     public function wc_session_expiring()
     {
         return time() +   (60 * 60 * 24 * 7) - (1 * 60 * 60);
     }
 
 
+    /**
+     * Woocommerce session expiration.
+     *
+     * @return int
+     */
     public function wc_session_expiration()
     {
         // 7 days
